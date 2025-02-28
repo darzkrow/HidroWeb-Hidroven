38740cacd5526f75eb38f9e730321eed39915f26
DarzKrow <gfrancojf@gmail.com>
2025-02-28 00:11:26 -0400

    modified:   config/urls.py

--- config/urls.py
+++ config/urls.py
@@ -16,6 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import handler404, handler403, handler500
+from .views import error_404, error_403, error_500
+
+handler404 = error_404
+handler403 = error_403
+handler500 = error_500
 
 
 urlpatterns = [
